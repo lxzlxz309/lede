374ea3bd0a11d3535c245fda2bbdba53232bc444
lxzlxz309 <lxzlxz309@gmail.com>
2024-07-03 18:56:01 +0800

    1

--- package/momoe/files/usr/lib/lua/luci/controller/momoe.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.momoe", package.seeall)
-
-function index()
-	local page
-	page = entry({"admin", "network", "momoe"}, cbi("momoe"), _("移远模块"), 100)
-	page.dependent = true
-	-- entry({"admin", "network", "momoe", "status"}, call("action_status"))
-	-- entry({"admin", "network", "momoe", "status"})
-end
